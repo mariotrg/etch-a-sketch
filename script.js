4f8e33dbd6a9ff18905c5c18bb1619489a27f299
Mario <132083365+mariotrg@users.noreply.github.com>
2023-04-30 20:49:26 +0100

    Generate 16*16 grid function added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const gridContainer = document.querySelector(".grid-container");
 
+const DEFAULT_SIZE = 16;
+
+function createGrid(size) {
+  gridContainer.innerHTML = "";
+
+  for (let i = 0; i < size * size; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+
+    gridContainer.append(cell);
+  }
+
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+window.onload = () => {
+  createGrid(DEFAULT_SIZE);
+};
