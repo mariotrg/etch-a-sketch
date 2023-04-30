4870a1e82d7b94ba16a69c7437fa8a24e8abfb00
Mario <132083365+mariotrg@users.noreply.github.com>
2023-04-30 21:28:06 +0100

    Implemented different modes in paint function, implemented set mode function and active mode function

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const gridContainer = document.querySelector(".grid-container");
 const sizePicker = document.querySelector(".size-picker");
 const sizeLabel = document.querySelector(".size-label");
+const modeBtn = document.querySelectorAll(".mode-btn");
+const colorPicker = document.querySelector("#color-picker");
+const clearBtn = document.querySelector(".clear-btn");
 
 const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = "color";
 
 let isDown = false;
+let currentMode = DEFAULT_MODE;
 
 gridContainer.addEventListener("mousedown", () => (isDown = true));
 gridContainer.addEventListener("mouseup", () => (isDown = false));
@@ -21,8 +26,20 @@ sizePicker.addEventListener("mouseup", () => {
   createGrid(sizePicker.value);
 });
 
-sizePicker.addEventListener("mousemove", () => {
+sizePicker.addEventListener("input", () => {
   updateSizeLabel(sizePicker.value);
+});
+
+modeBtn.forEach((button) => {
+  button.addEventListener("click", handleButtonClick);
+});
+
+colorPicker.addEventListener("input", () => {
+  setMode("color");
+});
+
+clearBtn.addEventListener("click", () => {
+  clearGrid();
 });
 
 function createGrid(size) {
@@ -40,8 +57,49 @@ function createGrid(size) {
   gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 }
 
+function handleButtonClick(e) {
+  setMode(e.target.value);
+}
+
+function setMode(mode) {
+  currentMode = mode;
+  setActive(mode);
+}
+
+function setActive(mode) {
+  modeBtn.forEach((button) => {
+    button.classList.remove("active");
+    if (button.value === mode) {
+      button.classList.add("active");
+    }
+  });
+}
+
 function paint(cell) {
-  cell.target.style.background = "#000000";
+  switch (currentMode) {
+    case "color":
+      cell.target.style.background = colorPicker.value;
+      break;
+    case "rainbow":
+      let r = random(0, 255);
+      let g = random(0, 255);
+      let b = random(0, 255);
+      cell.target.style.background = `rgb(${r}, ${g}, ${b})`;
+      break;
+    case "eraser":
+      cell.target.style.background = "#ffffff";
+      break;
+  }
+}
+
+function clearGrid() {
+  gridContainer.childNodes.forEach(
+    (cell) => (cell.style.background = "#ffffff")
+  );
+}
+
+function random(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function updateSizeLabel(size) {
@@ -50,4 +108,5 @@ function updateSizeLabel(size) {
 
 window.onload = () => {
   createGrid(DEFAULT_SIZE);
+  setMode(DEFAULT_MODE);
 };
